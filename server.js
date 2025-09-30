ece8065e1c592e3b6e60dc6691e8272049fc390e
jumman <mu.jumman405@gmail.com>
2025-09-30 09:40:28 +0000

    port env

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
 
+const port = process.env.PORT || 8080
+
 app.get('/', (req, res)=>{
     res.send('Hello Subtle')
 })
 
-app.listen(8080, ()=>{
+app.listen(port, ()=>{
     console.log('open port 80')
 })
